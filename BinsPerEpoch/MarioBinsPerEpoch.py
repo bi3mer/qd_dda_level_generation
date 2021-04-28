abaa53d91108594ffd52e7f6e401706b6c514974
bi3mer <colan@comcast.net>
2021-04-28 10:01:11 -0400

    Mario Agent and Test Level - mario agent updated based on Icarus agent - test_level.py implemented to test Mario levels and will be extended to the other games

--- BinsPerEpoch/MarioBinsPerEpoch.py
+++ BinsPerEpoch/MarioBinsPerEpoch.py
@@ -12,7 +12,6 @@ class MarioBinsPerEpoch(BinsPerEpoch):
 
         self.start_population_size = 500
         self.fast_iterations = 50000
-        self.fast_iterations = 5000
         self.slow_iterations = 0
         
         self.feature_names = ['linearity', 'leniency']
