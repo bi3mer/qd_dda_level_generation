e25a5e1cf444c2189ff54d8c18dcc316e2b01351
Seth Cooper <seth.cooper@gmail.com>
2021-05-01 15:22:31 -0400

    Add random seed argument.

--- me_average.py
+++ me_average.py
@@ -12,7 +12,8 @@ group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('--dungeongram', action='store_true', help='Run DungeonGrams')
 group.add_argument('--mario', action='store_true', help='Run Mario')
 group.add_argument('--icarus', action='store_true', help='Run Icarus')
-parser.add_argument('--runs',type=check_positive, help='Number of runs.', required=True)
+parser.add_argument('--runs', type=check_positive, help='Number of runs.', required=True)
+parser.add_argument('--seed', type=check_positive, help='Random seed.', default=0)
 args = parser.parse_args()
 
 if args.dungeongram:
@@ -21,5 +22,7 @@ elif args.mario:
     runner = MarioBinsPerEpoch()
 elif args.icarus:
     runner = IcarusBinsPerEpoch()
+
+runner.seed = args.seed
 
 runner.run(args.runs)
